d67740ea22fab665dd2e34fa2ac5ef8927f98a45
Max <pilot.max777@gmail.com>
2023-02-10 17:03:24 -0800

    Initial Commit

---
+++ WantToCryEngine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantToCryEngine
+//
+//  Created by Alex on 2023-02-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
